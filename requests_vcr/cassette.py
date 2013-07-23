14eaf96264f6d5f0e908db6c258b33bd2bd63de3
Ian Cordasco <graffatcolmingov@gmail.com>
2013-07-23 12:53:52 -0500

    Work on serialization

--- requests_vcr/cassette.py
+++ requests_vcr/cassette.py
@@ -2,14 +2,22 @@ import json
 from requests.models import Response
 
 
+def serialize_prepared_request(request, method):
+    pass
+
+
+def serialize_response(response, method):
+    pass
+
+
 class Cassette(object):
     def __init__(self, cassette_name, serialize, mode='rb'):
         self.cassette_name = cassette_name
         self.serialize_format = serialize
-        self.response = Response()
+        self.recorded_response = Response()
         self.fd = open(cassette_name, mode)
 
-    def serialize(self):
+    def serialize(self, response):
         if self.serialize == 'json':
             data = {
                 'request': {},
