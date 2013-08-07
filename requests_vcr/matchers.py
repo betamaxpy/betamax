089be38ac2c25bec1e2a367eb040996ff4c5ba42
Ian Cordasco <graffatcolmingov@gmail.com>
2013-08-06 22:09:02 -0500

    Implement an awesome BaseMatcher
    Also start using it to implement vcr's default matchers
    
    This is the best API I could think of. Going to bug Kenneth for his opinion.

---
+++ requests_vcr/matchers.py
@@ -0,0 +1,89 @@
+from requests.compat import urlparse
+matcher_registry = {}
+
+
+class BaseMatcher(type):
+
+    """
+    Base class that ensures sub-classes that implement custom matchers can be
+    registered and have the only method that is required.
+
+    Usage::
+
+        from requests_vcr.matchers import BaseMatcher
+
+        class MyMatcher(BaseMatcher.metaclass()):
+            name = 'my'
+
+            def match(self, request, recorded_request):
+                # My fancy matching algorithm
+
+        MyMatcher()
+
+    The `match` method will be given a `requests.PreparedRequest` object and a
+    dictionary. The dictionary always has the following keys:
+
+    - url
+    - method
+    - body
+    - headers
+
+    .. note:: The above usage ensures that your matcher will work across
+        Python 2.6 - 3.3. If you're only targetting one major version of
+        Python, you can simply use the appropriate metaclass declaration
+        with BaseMatcher, e.g.,
+
+        ::
+
+            # Python 3
+            class MyMatcher(metaclass=BaseMatcher):
+                pass
+
+            # Python 2
+            class MyMatcher(object):
+                __metaclass__ = BaseMatcher
+
+    """
+
+    name = None
+
+    def __new__(cls, name, bases, attrs):
+        new_cls = type.__new__(cls, name, bases, attrs)
+
+        if 'metaclass' in attrs:
+            del attrs['metaclass']
+
+        matcher_registry[attrs.get('name')] = new_cls()
+        return new_cls
+
+    @staticmethod
+    def metaclass(*bases):
+        """Remove the need for ``six.with_metaclass``"""
+        return BaseMatcher('BaseMatcher', bases, {})
+
+
+class URIMatcher(BaseMatcher.metaclass()):
+    name = 'uri'
+
+    def match(self, request, recorded_request):
+        return request.url == recorded_request['url']
+
+
+class MethodMatcher(BaseMatcher.metaclass()):
+    name = 'method'
+
+    def match(self, request, recorded_request):
+        return request.method == recorded_request['method']
+
+
+class HostMatcher(BaseMatcher.metaclass()):
+    name = 'host'
+
+    def match(self, request, recorded_request):
+        request_host = urlparse(request.url).host
+        recorded_host = urlparse(recorded_request['url']).host
+        return request_host == recorded_host
+
+
+if None in matcher_registry:
+    del matcher_registry[None]
