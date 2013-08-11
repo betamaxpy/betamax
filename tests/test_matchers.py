0524c1bb48ffa56f54ae2cbc6f71f7624d65744a
Ian Cordasco <graffatcolmingov@gmail.com>
2013-08-10 21:37:19 -0500

    Test the BaseMatcher class

--- tests/test_matchers.py
+++ tests/test_matchers.py
@@ -1,7 +1,7 @@
 import unittest
 
 from requests import PreparedRequest
-from requests_vcr.matchers import matcher_registry
+from requests_vcr import matchers
 
 
 class TestMatchers(unittest.TestCase):
@@ -15,50 +15,50 @@ class TestMatchers(unittest.TestCase):
         self.p.method = 'GET'
 
     def test_matcher_registry_has_body_matcher(self):
-        assert 'body' in matcher_registry
+        assert 'body' in matchers.matcher_registry
 
     def test_matcher_registry_has_headers_matcher(self):
-        assert 'headers' in matcher_registry
+        assert 'headers' in matchers.matcher_registry
 
     def test_matcher_registry_has_host_matcher(self):
-        assert 'host' in matcher_registry
+        assert 'host' in matchers.matcher_registry
 
     def test_matcher_registry_has_method_matcher(self):
-        assert 'method' in matcher_registry
+        assert 'method' in matchers.matcher_registry
 
     def test_matcher_registry_has_path_matcher(self):
-        assert 'path' in matcher_registry
+        assert 'path' in matchers.matcher_registry
 
     def test_matcher_registry_has_query_matcher(self):
-        assert 'query' in matcher_registry
+        assert 'query' in matchers.matcher_registry
 
     def test_matcher_registry_has_uri_matcher(self):
-        assert 'uri' in matcher_registry
+        assert 'uri' in matchers.matcher_registry
 
     def test_body_matcher(self):
-        match = matcher_registry['body'].match
+        match = matchers.matcher_registry['body'].match
         assert match(self.p, {'body': 'Foo bar'})
         assert match(self.p, {'body': ''}) is False
 
     def test_headers_matcher(self):
-        match = matcher_registry['headers'].match
+        match = matchers.matcher_registry['headers'].match
         assert match(self.p, {'headers': {'User-Agent': 'requests-vcr/test'}})
         assert match(self.p, {'headers': {'X-Sha': '6bbde0af'}}) is False
 
     def test_host_matcher(self):
-        match = matcher_registry['host'].match
+        match = matchers.matcher_registry['host'].match
         assert match(self.p, {'url': 'http://example.com'})
         assert match(self.p, {'url': 'https://example.com'})
         assert match(self.p, {'url': 'https://example.com/path'})
         assert match(self.p, {'url': 'https://example2.com'}) is False
 
     def test_method_matcher(self):
-        match = matcher_registry['method'].match
+        match = matchers.matcher_registry['method'].match
         assert match(self.p, {'method': 'GET'})
         assert match(self.p, {'method': 'POST'}) is False
 
     def test_path_matcher(self):
-        match = matcher_registry['path'].match
+        match = matchers.matcher_registry['path'].match
         assert match(self.p, {'url': 'http://example.com/path/to/end/point'})
         assert match(self.p,
                      {'url': 'http://example.com:8000/path/to/end/point'})
@@ -66,7 +66,7 @@ class TestMatchers(unittest.TestCase):
                      {'url': 'http://example.com:8000/path/to/end/'}) is False
 
     def test_query_matcher(self):
-        match = matcher_registry['query'].match
+        match = matchers.matcher_registry['query'].match
         assert match(
             self.p,
             {'url': 'http://example.com/path/to/end/point?query=string'}
@@ -89,9 +89,24 @@ class TestMatchers(unittest.TestCase):
         assert match(self.p, {'url': 'http://example.com'}) is False
 
     def test_uri_matcher(self):
-        match = matcher_registry['uri'].match
+        match = matchers.matcher_registry['uri'].match
         assert match(
             self.p,
             {'url': 'http://example.com/path/to/end/point?query=string'}
         )
         assert match(self.p, {'url': 'http://example.com'}) is False
+
+
+class TestBaseMatcher(unittest.TestCase):
+    def setUp(self):
+        class Matcher(matchers.BaseMatcher):
+            pass
+        self.Matcher = Matcher
+
+    def test_requires_name(self):
+        self.assertRaises(ValueError, self.Matcher)
+
+    def test_requires_you_overload_match(self):
+        self.Matcher.name = 'test'
+        m = self.Matcher()
+        self.assertRaises(NotImplementedError, m.match, None, None)
