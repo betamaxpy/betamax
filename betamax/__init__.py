41fd7b71dc6a9f05fffafac590526f38dbccc217
Ian Cordasco <graffatcolmingov@gmail.com>
2013-08-15 11:43:29 -0500

    Setup setup.py and version info

--- betamax/__init__.py
+++ betamax/__init__.py
@@ -2,3 +2,4 @@ from betamax.vcr import VCR
 from betamax.matchers import BaseMatcher
 
 __all__ = [VCR, BaseMatcher]
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,46 @@
+import re
+from setuptools import setup
+
+packages = ['betamax']
+requires = ['requests >= 1.2.3']
+
+__version__ = ''
+with open('betamax/__init__.py', 'r') as fd:
+    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    for line in fd:
+        m = reg.match(line)
+        if m:
+            __version__ = m.group(1)
+            break
+
+if not __version__:
+    raise RuntimeError('Cannot find version information')
+
+setup(
+    name="betamax",
+    version=__version__,
+    description="A VCR imitation for python-requests",
+    long_description="\n\n".join([open("README.rst").read(),
+                                  open("HISTORY.rst").read()]),
+    license=open('LICENSE').read(),
+    author="Ian Cordasco",
+    author_email="graffatcolmingov@gmail.com",
+    url="https://github.com/sigmavirus24/betamax",
+    packages=packages,
+    package_data={'': ['LICENSE', 'AUTHORS.rst']},
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
