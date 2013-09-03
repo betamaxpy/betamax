1d80ebf942a022beafb752cc5917fead4a7bc204
Ian Cordasco <graffatcolmingov@gmail.com>
2013-09-02 19:45:32 -0500

    Add test to ensure responses are identical

--- tests/integration/test_record_modes.py
+++ tests/integration/test_record_modes.py
@@ -12,3 +12,16 @@ class TestBetamax(IntegrationHelper):
             assert r.status_code == 200
             assert betamax.current_cassette.is_empty() is False
             self.cassette_path = betamax.current_cassette.cassette_name
+
+    def test_replays_response(self):
+        s = self.session
+        with Betamax(s).use_cassette('test_replays_response') as betamax:
+            assert betamax.current_cassette.is_empty() is True
+            r0 = s.get('http://httpbin.org/get')
+            assert r0.status_code == 200
+            assert betamax.current_cassette.is_empty() is False
+            r1 = s.get('http://httpbin.org/get')
+            assert r1.status_code == 200
+            assert r0.headers == r1.headers
+            assert r0.content == r1.content
+            self.cassette_path = betamax.current_cassette.cassette_name
