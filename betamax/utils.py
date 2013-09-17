08e12f807edebf0243813560005be14734eb9c2f
Ian Cordasco <graffatcolmingov@gmail.com>
2013-09-16 22:51:06 -0500

    Don't pass None to str.encode either

--- betamax/utils.py
+++ betamax/utils.py
@@ -4,7 +4,10 @@ from requests.compat import is_py2
 
 def coerce_content(content, encoding):
     if hasattr(content, 'decode') and not is_py2:
-        return content.decode(encoding) if encoding else content.decode()
+        if encoding:
+            content = content.decode(encoding)
+        else:
+            content = content.decode()
     return content
 
 
@@ -12,5 +15,8 @@ def body_io(string, encoding):
     if is_py2:
         return io.StringIO(string)
     if hasattr(string, 'encode'):
-        string = string.encode(encoding)
+        if encoding:
+            string = string.encode(encoding)
+        else:
+            string = string.encode()
     return io.BytesIO(string)
