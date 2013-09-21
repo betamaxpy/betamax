935b642ffab8af6ed27d19437d811522a1e7cddb
Ian Cordasco <graffatcolmingov@gmail.com>
2013-09-21 10:04:22 -0500

    Fix encoding bug
    Should have been more thorough in my integration tests

--- betamax/utils.py
+++ betamax/utils.py
@@ -1,5 +1,4 @@
 import io
-from requests.compat import is_py2
 
 
 def coerce_content(content, encoding=None):
@@ -9,8 +8,6 @@ def coerce_content(content, encoding=None):
 
 
 def body_io(string, encoding=None):
-    if is_py2:
-        return io.StringIO(string)
     if hasattr(string, 'encode'):
         string = string.encode(encoding or 'utf-8')
     return io.BytesIO(string)
