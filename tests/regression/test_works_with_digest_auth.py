0123c32f1553a05e5fb9cb87eb66fd8fa15a5bd7
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-03 09:17:47 -0500

    Clean up tests

--- tests/regression/test_works_with_digest_auth.py
+++ tests/regression/test_works_with_digest_auth.py
@@ -1,44 +1,15 @@
 import unittest
 
 from betamax import Betamax
-from betamax.matchers import HeadersMatcher
 from requests import Session
 from requests.auth import HTTPDigestAuth
-
-
-class CustomHeadersMatcher(HeadersMatcher):
-    name = 'custom-headers'
-
-    def match(self, request, recorded_request):
-        request_headers = dict(request.headers)
-        recorded_headers = self.flatten_headers(recorded_request)
-        self.remove_unwanted_headers(request_headers)
-        self.remove_unwanted_headers(recorded_headers)
-        return request_headers == recorded_headers
-
-    def _remove_header(self, header, headers):
-        headers.pop(header, None)
-        headers.pop(header.lower(), None)
-
-    def remove_user_agent(self, headers):
-        self._remove_header('User-Agent', headers)
-
-    def remove_authorization(self, headers):
-        self._remove_header('Authorization', headers)
-
-    def remove_unwanted_headers(self, headers):
-        self.remove_user_agent(headers)
-        self.remove_authorization(headers)
-
-
-Betamax.register_request_matcher(CustomHeadersMatcher)
 
 
 class TestDigestAuth(unittest.TestCase):
     def test_saves_content_as_gzip(self):
         s = Session()
         cassette_name = 'handles_digest_auth'
-        match = ['method', 'uri', 'custom-headers', 'digest-auth']
+        match = ['method', 'uri', 'digest-auth']
         with Betamax(s).use_cassette(cassette_name, match_requests_on=match):
             r = s.get('https://httpbin.org/digest-auth/auth/user/passwd',
                       auth=HTTPDigestAuth('user', 'passwd'))
