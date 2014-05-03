6ffc3de5cf0b75d6ba2866a60aebdbc51cb684ac
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-03 08:58:37 -0500

    Add DigestAuthMatcher

--- betamax/matchers/__init__.py
+++ betamax/matchers/__init__.py
@@ -2,6 +2,7 @@ matcher_registry = {}
 
 from .base import BaseMatcher
 from .body import BodyMatcher
+from .digest_auth import DigestAuthMatcher
 from .headers import HeadersMatcher
 from .host import HostMatcher
 from .method import MethodMatcher
@@ -10,11 +11,12 @@ from .query import QueryMatcher
 from .uri import URIMatcher
 
 
-__all__ = ['BaseMatcher', 'BodyMatcher', 'HeadersMatcher', 'HostMatcher',
-           'MethodMatcher', 'PathMatcher', 'QueryMatcher', 'URIMatcher']
+__all__ = ['BaseMatcher', 'BodyMatcher', 'DigestAuthMatcher',
+           'HeadersMatcher', 'HostMatcher', 'MethodMatcher', 'PathMatcher',
+           'QueryMatcher', 'URIMatcher']
 
 
-_matchers = [BodyMatcher, HeadersMatcher, HostMatcher, MethodMatcher,
-             PathMatcher, QueryMatcher, URIMatcher]
+_matchers = [BodyMatcher, DigestAuthMatcher, HeadersMatcher, HostMatcher,
+             MethodMatcher, PathMatcher, QueryMatcher, URIMatcher]
 matcher_registry.update(dict((m.name, m()) for m in _matchers))
 del _matchers
