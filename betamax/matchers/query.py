0c1c38b2a05ee1fcdd3d3cd2f2b6acc008047e6a
Ian Cordasco <graffatcolmingov@gmail.com>
2014-05-14 08:08:28 -0500

    Fix #33

--- betamax/matchers/query.py
+++ betamax/matchers/query.py
@@ -2,6 +2,11 @@
 from .base import BaseMatcher
 from requests.compat import urlparse
 
+try:
+    from urlparse import parse_qs
+except ImportError:
+    from urllib.parse import parse_qs
+
 
 class QueryMatcher(BaseMatcher):
     # Matches based on the query of the request
@@ -9,9 +14,7 @@ class QueryMatcher(BaseMatcher):
 
     def to_dict(self, query):
         """Turn the query string into a dictionary"""
-        if not query:
-            return {}
-        return dict(q.split('=') for q in query.split('&'))
+        return parse_qs(query or '')  # Protect against None
 
     def match(self, request, recorded_request):
         request_query = self.to_dict(urlparse(request.url).query)
