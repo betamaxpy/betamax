cf6aefee2bb3da56186dee1199d915ced11c5404
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2018-03-13 17:41:56 -0500

    Move betamax into the src directory
    Let's be sure that we're testing the package and not the local copy when
    we run our tests.

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+"""Packaging logic for betamax."""
 import os
 import re
 import sys
-from setuptools import setup, find_packages
 
-packages = find_packages(exclude=['tests', 'tests.integration'])
-requires = ['requests >= 2.0']
+import setuptools
 
-__version__ = ''
-with open('betamax/__init__.py', 'r') as fd:
-    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+packages = setuptools.find_packages(
+    "src",
+    exclude=["tests", "tests.integration"],
+)
+requires = ["requests >= 2.0"]
+
+__version__ = ""
+with open("src/betamax/__init__.py", "r") as fd:
+    reg = re.compile(r"__version__ = [\'']([^\'']*)[\'']")
     for line in fd:
         m = reg.match(line)
         if m:
@@ -16,19 +21,21 @@ with open('betamax/__init__.py', 'r') as fd:
             break
 
 if not __version__:
-    raise RuntimeError('Cannot find version information')
+    raise RuntimeError("Cannot find version information")
 
-if sys.argv[-1] in ['submit', 'publish']:
+if sys.argv[-1] in ["submit", "publish"]:
     os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
 
 
 def data_for(filename):
+    """Read the file data for a filename."""
     with open(filename) as fd:
         content = fd.read()
     return content
 
-setup(
+
+setuptools.setup(
     name="betamax",
     version=__version__,
     description="A VCR imitation for python-requests",
@@ -39,23 +46,24 @@ setup(
     author_email="graffatcolmingov@gmail.com",
     url="https://github.com/sigmavirus24/betamax",
     packages=packages,
-    package_data={'': ['LICENSE', 'AUTHORS.rst']},
+    package_dir={"": "src"},
+    package_data={"": ["LICENSE", "AUTHORS.rst"]},
     include_package_data=True,
     install_requires=requires,
     entry_points={
-        'pytest11': ['pytest-betamax = betamax.fixtures.pytest']
+        "pytest11": ["pytest-betamax = betamax.fixtures.pytest"]
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
     ]
 )
