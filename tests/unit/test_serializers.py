13ba6c70d59001e8349b6f0a030a39b470446cc0
Ian Stapleton Cordasco <graffatcolmingov@gmail.com>
2018-03-14 06:37:46 -0500

    Add tests for stored_as_binary support
    This also cleans up the serializers unit tests by adding docstrings and
    only importing modules.

--- tests/unit/test_serializers.py
+++ tests/unit/test_serializers.py
@@ -1,42 +1,110 @@
+"""Tests for serializers."""
 import os
-import pytest
 import unittest
 
-from betamax.serializers import BaseSerializer, JSONSerializer
+import mock
+import pytest
+
+from betamax.serializers import base
+from betamax.serializers import json_serializer
+from betamax.serializers import proxy
 
 
 class TestJSONSerializer(unittest.TestCase):
+    """Tests around the JSONSerializer default."""
+
     def setUp(self):
+        """Fixture setup."""
         self.cassette_dir = 'fake_dir'
         self.cassette_name = 'cassette_name'
 
     def test_generate_cassette_name(self):
+        """Verify the beaviour of generate_cassette_name."""
         assert (os.path.join('fake_dir', 'cassette_name.json') ==
-                JSONSerializer.generate_cassette_name(self.cassette_dir,
-                                                      self.cassette_name))
+                json_serializer.JSONSerializer.generate_cassette_name(
+                    self.cassette_dir,
+                    self.cassette_name,
+                ))
 
     def test_generate_cassette_name_with_instance(self):
-        serializer = JSONSerializer()
+        """Verify generate_cassette_name works on an instance too."""
+        serializer = json_serializer.JSONSerializer()
         assert (os.path.join('fake_dir', 'cassette_name.json') ==
                 serializer.generate_cassette_name(self.cassette_dir,
                                                   self.cassette_name))
 
 
-class Serializer(BaseSerializer):
+class Serializer(base.BaseSerializer):
+    """Serializer to test NotImplementedError exceptions."""
+
     name = 'test'
 
 
+class BytesSerializer(base.BaseSerializer):
+    """Serializer to test stored_as_binary."""
+
+    name = 'bytes-test'
+    stored_as_binary = True
+
+    # NOTE(sigmavirus24): These bytes, when decoded, result in a
+    # UnicodeDecodeError
+    serialized_bytes = b"hi \xAD"
+
+    def serialize(self, *args):
+        """Return the problematic bytes."""
+        return self.serialized_bytes
+
+    def deserialize(self, *args):
+        """Return the problematic bytes."""
+        return self.serialized_bytes
+
+
 class TestBaseSerializer(unittest.TestCase):
+    """Tests around BaseSerializer behaviour."""
+
     def test_serialize_is_an_interface(self):
+        """Verify we handle unimplemented methods."""
         serializer = Serializer()
         with pytest.raises(NotImplementedError):
             serializer.serialize({})
 
     def test_deserialize_is_an_interface(self):
+        """Verify we handle unimplemented methods."""
         serializer = Serializer()
         with pytest.raises(NotImplementedError):
             serializer.deserialize('path')
 
     def test_requires_a_name(self):
+        """Verify we handle unimplemented methods."""
         with pytest.raises(ValueError):
-            BaseSerializer()
+            base.BaseSerializer()
+
+
+class TestBinarySerializers(unittest.TestCase):
+    """Verify the behaviour of stored_as_binary."""
+
+    @pytest.fixture(autouse=True)
+    def _setup(self):
+        serializer = BytesSerializer()
+        self.cassette_path = 'test_cassette.test'
+        self.proxy = proxy.SerializerProxy(
+            serializer,
+            self.cassette_path,
+            allow_serialization=True,
+        )
+
+    def test_serialize(self):
+        """Verify we use the right mode with open()."""
+        mopen = mock.mock_open()
+        with mock.patch('betamax.serializers.proxy.open', mopen):
+            self.proxy.serialize({'cassette': 'data'})
+
+        assert mock.call(self.cassette_path, 'wb') in mopen.mock_calls
+
+    def test_deserialize(self):
+        """Verify we use the right mode with open()."""
+        mopen = mock.mock_open()
+        with mock.patch('betamax.serializers.proxy.open', mopen):
+            self.proxy.deserialize()
+
+        assert mock.call(self.cassette_path, 'rb') in mopen.mock_calls
